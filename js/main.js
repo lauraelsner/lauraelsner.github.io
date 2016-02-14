07be5f0ce1762beb24c8d0698598efffbae18ef8
lauraelsner <lauraelsner@Lauras-MacBook-Pro-2.local>
2016-02-14 14:06:08 -0600

    need to test live

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 
-// -------------------------------------------------------------------------------
-// Part 1 - Click a box! Get them all!
-// -------------------------------------------------------------------------------
-
-// 1. When an element with the class .box is clicked on, fade that .box out.
-
-// 2. When an element with the class .slide-me is clicked on, slide that .slide-me up.
-
-
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
